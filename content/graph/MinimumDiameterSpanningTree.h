6f983b374a34fea5407705cf609e42ababd7ddf4
Le Bao Hiep <baohiep2013@gmail.com>
2020-10-28 22:55:33 +0700

    Update MinimumDiameterSpanningTree

--- content/graph/MinimumDiameterSpanningTree.h
+++ content/graph/MinimumDiameterSpanningTree.h
@@ -1,58 +1,46 @@
 /**
  * Author: hieplpvip
- * Date: 2020-10-17
+ * Date: 2020-10-28
  * License: CC0
  * Source: https://web.archive.org/web/20160309101249/https://adn.botao.hu/adn-backup/blog/attachments/month_0705/32007531153238.pdf
- * Description: Finds a minimum diameter spanning tree
+ * Description: Find a minimum diameter spanning tree
+ * Usage: Call init() first. Add edge and call findMDST(). Return diameter.
  * Time: $O(n^3)$
- * Status: untested
+ * Status: Tested on https://codeforces.com/contest/266/problem/D
  */
 #pragma once
 
-const ll oo = 1e18;
-const int N = 500;
+const int N = 500, oo = 1e9;
 
 struct Edge {
   int u, v, w;
-  Edge(int _u, int _v, int _w): u(_u), v(_v), w(_w) {}
 };
 
-int n, m;
-ll d[N][N], ans = oo;
+int n, m, d[N][N], ans = oo;
 vector<int> que[N];
 vector<Edge> edges;
 
-int main() {
-  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-  cin >> n >> m;
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 1; j <= n; ++j) {
-      d[i][j] = (i == j) ? 0 : oo;
-    }
-  }
-  for (int i = 1, u, v, w; i <= m; ++i) {
-    cin >> u >> v >> w;
-    w *= 2;
-    d[u][v] = d[v][u] = min(d[u][v], (ll)w);
-    edges.emplace_back(u, v, w);
-  }
-  for (int k = 1; k <= n; ++k) {
-    for (int u = 1; u <= n; ++u) {
-      for (int v = 1; v <= n; ++v) {
-        d[u][v] = min(d[u][v], d[u][k] + d[k][v]);
-      }
-    }
-  }
-  for (int i = 1; i <= n; ++i) {
+inline void init() {
+  rep(i,1,n+1) rep(j,1,n+1) d[i][j] = (i == j) ? 0 : oo;
+}
+inline void addEdge(int u, int v, int w) {
+  w *= 2; d[u][v] = d[v][u] = min(d[u][v], w);
+  edges.pb({u, v, w});
+}
+int findMDST() {
+  rep(k,1,n+1) rep(u,1,n+1) rep(v,1,n+1)
+    d[u][v] = min(d[u][v], d[u][k] + d[k][v]);
+  rep(i,1,n+1) {
     que[i].resize(n);
-    iota(que[i].begin(), que[i].end(), 1);
-    sort(que[i].begin(), que[i].end(), [&](const int &a, const int &b) {
+    iota(all(que[i]), 1);
+    sort(all(que[i]), [&](const int &a, const int &b) {
       return d[i][a] > d[i][b];
     });
   }
+  int ans = oo;
   for (auto &e: edges) {
     int &u = e.u, &v = e.v, cur = que[u][0];
-    ll w = e.w, tans = oo;
+    int w = e.w, tans = oo;
     tans = min({tans, d[u][cur], d[v][cur] + w});
     for (int &z: que[u]) {
       if (d[v][cur] < d[v][z]) {
@@ -63,6 +51,5 @@ int main() {
     tans = min({tans, d[v][cur], d[u][cur] + w});
     ans = min(ans, tans);
   }
-  cout << fixed << setprecision(2) << (double)ans / 2.0;
-  return 0;
+  return ans;
 }

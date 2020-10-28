2647fa6d3ad673100e8cdf762068af002706bb65
Le Bao Hiep <baohiep2013@gmail.com>
2020-10-28 11:25:40 +0700

    Fix previous commit

--- stress-tests/strings/SuffixAutomaton.cpp
+++ stress-tests/strings/SuffixAutomaton.cpp
@@ -44,15 +44,6 @@ struct Automaton {
   }
 };
 
-int minRotation(string s) {
-	int a=0, N=sz(s); s += s;
-	rep(b,0,N) rep(k,0,N) {
-		if (a+k == b || s[a+k] < s[b+k]) {b += max(0, k-1); break;}
-		if (s[a+k] > s[b+k]) { a = b; break; }
-	}
-	return a;
-}
-
 int main() {
   srand(8);
   rep(N,5,10000) {

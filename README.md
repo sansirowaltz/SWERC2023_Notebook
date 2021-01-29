5412d78b01519102292a634c246261ef7a11ec56
Le Bao Hiep <baohiep2013@gmail.com>
2021-01-30 00:01:55 +0700

    Change team name

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# HCMUS-HHHH ICPC Notebook
+# HCMUS-EternalBlue ICPC Notebook
 
-This repo hosts ICPC Notebook of team HCMUS-HHHH. It is based on [kactl](https://github.com/kth-competitive-programming/kactl) and consists of 25 pages of copy-pasteable C++ code, for use in ICPC-style programming competitions.
+This repo hosts ICPC Notebook of team HCMUS-EternalBlue. It is based on [kactl](https://github.com/kth-competitive-programming/kactl) and consists of 25 pages of copy-pasteable C++ code, for use in ICPC-style programming competitions.
 
 See [hcmus.pdf](./hcmus.pdf) for the final, browsable version, and [content](./content/) for raw source code.
--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,19 +1,21 @@
 //#pragma GCC optimize("Ofast")
 //#pragma GCC target("avx2")
 #include <bits/stdc++.h>
-using namespace std;
-
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 #define uniq(x) x.resize(unique(all(x)) - x.begin())
+#define ff first
+#define ss second
 #define pb push_back
 #define emb emplace_back
+using namespace std;
 using ull = unsigned long long;
 using ll = long long;
 using pii = pair<int, int>;
 using vi = vector<int>;
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  return 0;
 }

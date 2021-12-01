38af4614ea34bd12a01d015039b0859cc50536ed
Le Bao Hiep <baohiep2013@gmail.com>
2021-12-01 13:53:47 +0700

    Add persistent segment tree

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -8,18 +8,19 @@
  * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
  * Time: O(\log N).
  * Usage: Node* tr = new Node(v, 0, sz(v));
- * Status: stress-tested a bit
+ * Status: stress-tested a bit. tested persistence on CSES Range Queries and Copies.
  */
 #pragma once
 
 #include "../various/BumpAllocator.h"
 
 const int inf = 1e9;
+const bool PERSISTENT = false; // set to true for persistence
 struct Node {
   Node *l = 0, *r = 0;
   int lo, hi, mset = inf, madd = 0, val = -inf;
   Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
-  Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
+  Node(vi& v, int lo, int hi): lo(lo), hi(hi) {
     if (lo + 1 < hi) {
       int mid = lo + (hi - lo)/2;
       l = new Node(v, lo, mid); r = new Node(v, mid, hi);
@@ -33,34 +34,43 @@ struct Node {
     push();
     return max(l->query(L, R), r->query(L, R));
   }
-  void set(int L, int R, int x) {
-    if (R <= lo || hi <= L) return;
-    if (L <= lo && hi <= R) mset = val = x, madd = 0;
-    else {
-      push(), l->set(L, R, x), r->set(L, R, x);
-      val = max(l->val, r->val);
-    }
-  }
-  void add(int L, int R, int x) {
-    if (R <= lo || hi <= L) return;
+  Node* set(int L, int R, int x, bool persist = PERSISTENT) {
+    if (R <= lo || hi <= L) return this;
+    Node* res = persist ? new Node(*this) : this;
     if (L <= lo && hi <= R) {
-      if (mset != inf) mset += x;
-      else madd += x;
-      val += x;
+      res->mset = res->val = x;
+      res->madd = 0;
+    } else {
+      res->push();
+      res->l=res->l->set(L,R,x),res->r=res->r->set(L,R,x);
+      res->val = max(res->l->val, res->r->val);
     }
-    else {
-      push(), l->add(L, R, x), r->add(L, R, x);
-      val = max(l->val, r->val);
+    return res;
+  }
+  Node* add(int L, int R, int x, bool persist = PERSISTENT) {
+    if (R <= lo || hi <= L) return this;
+    Node* res = persist ? new Node(*this) : this;
+    if (L <= lo && hi <= R) {
+      if (res->mset != inf) res->mset += x;
+      else res->madd += x;
+      res->val += x;
+    } else {
+      res->push();
+      res->l=res->l->add(L,R,x),res->r=res->r->add(L,R,x);
+      res->val = max(res->l->val, res->r->val);
     }
+    return res;
   }
   void push() {
+    bool nw = PERSISTENT;
     if (!l) {
+      nw = false;
       int mid = lo + (hi - lo)/2;
       l = new Node(lo, mid); r = new Node(mid, hi);
     }
     if (mset != inf)
-      l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
+      l=l->set(lo,hi,mset,nw),r=r->set(lo,hi,mset,nw),mset=inf;
     else if (madd)
-      l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+      l=l->add(lo,hi,madd,nw),r=r->add(lo,hi,madd,nw),madd=0;
   }
 };
